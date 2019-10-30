b13e77b10539dab9b4bea3a8c000c281dd143547
AlexisLeandroVallejos <alexv4lle@gmail.com>
2019-10-30 18:19:15 -0300

    Agregados tests de ubicacionDeJugadorOVillano en CarmenSanDiegoMasterTest

--- tests/test/CarmenSanDiegoMasterTest.java
+++ tests/test/CarmenSanDiegoMasterTest.java
@@ -1,8 +1,14 @@
 package test;
 
 import excepciones.NoEsPaisProximoException;
+import excepciones.NoHayLugarDeInteresEnEstePais;
 import modelo.*;
+import modelo.lugarInteres.Banco;
+import modelo.lugarInteres.Club;
+import modelo.lugarInteres.Embajada;
+import modelo.lugarInteres.LugarInteres;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.Arrays;
 import java.util.List;
@@ -109,5 +115,39 @@ public class CarmenSanDiegoMasterTest {
         carmen.setJugador(jugador);
         carmen.setVillano(villano);
         carmen.viajar(paisDeLasMaravillas);
+    }
+
+    @Test
+    public void ubicacionDeJugadorOVillano_seSeteaUnLugarDeInteresAUnaPersonaPorqueExisteEnElPais(){
+        CarmenSanDiegoMaster carmenSanDiegoMaster = new CarmenSanDiegoMaster();
+        Caso caso = new Caso();
+        Pais pais = new Pais();
+        caso.setPaisOrigen(pais);
+        Embajada embajada = mock(Embajada.class);
+        Club club = mock(Club.class);
+        List<LugarInteres> lugares = Arrays.asList(embajada, club);
+        Jugador jugador = new Jugador(caso);
+        pais.setLugaresInteres(lugares);
+
+        carmenSanDiegoMaster.ubicacionDeJugadorOVillano(embajada, jugador);
+
+        assertEquals(embajada, jugador.getLugarInteresActual());
+    }
+
+    @Test(expected = NoHayLugarDeInteresEnEstePais.class)
+    public void ubicacionDeJugadorOVillano_NoSeSeteaUnLugarDeInteresAUnaPersonaPorqueNoExisteEnElPais(){
+        CarmenSanDiegoMaster carmenSanDiegoMaster = new CarmenSanDiegoMaster();
+        Caso caso = new Caso();
+        Pais pais = new Pais();
+        caso.setPaisOrigen(pais);
+        Embajada embajada = mock(Embajada.class);
+        Club club = mock(Club.class);
+        List<LugarInteres> lugares = Arrays.asList(embajada, club);
+        Jugador jugador = new Jugador(caso);
+        pais.setLugaresInteres(lugares);
+
+        Banco noExisteEnPais = new Banco();
+
+        carmenSanDiegoMaster.ubicacionDeJugadorOVillano(noExisteEnPais, jugador);
     }
 }
