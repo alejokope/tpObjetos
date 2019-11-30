94cfdc9d420a3b2f8e12006cb266be4e6dc39fbe
AlexisLeandroVallejos <alexv4lle@gmail.com>
2019-11-30 20:26:16 -0300

    mando comentario por incompatiblidad

--- src/vista/JugandoCaso.java
+++ src/vista/JugandoCaso.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 public class JugandoCaso extends JFrame {
+	//blablabla
 
 	private JPanel contentPane;
 	private ResolviendoElCasoViewModel modelo = new ResolviendoElCasoViewModel();
